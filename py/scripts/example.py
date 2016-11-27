f9ce1c7a17617768ce0e9276e56df533f6d56492
Wade2016 <lyuwade@gmail.com>
2016-11-27 19:34:42 +0100

    example python code

---
+++ py/scripts/example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+cgitb.enable()
+
+print("Content-Type: text/plain;charset=utf-8")
+print(\n)
+
+print("Hello World!")
