bbfb601d297fed0c4e3d38b875e6053b4d0702c1
a1ultima <a1ultima@gmail.com>
2016-12-09 17:35:44 +0200

    dnds.py now runs on command-line, and via the Python server: start_server.py, returning data to the webpage: vb-genes.py via ajax

--- py/scripts/start_server.py
+++ py/scripts/start_server.py
@@ -1,13 +1,51 @@
 #!/usr/bin/python
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import BaseHTTPServer
+import pdb
+
+import dnds
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
+    # def end_headers (self):
+    #     self.send_header('Access-Control-Allow-Origin', '*')
+    #     self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    #     self.send_header('Access-Control-Allow-Headers', '*')
+    #     SimpleHTTPRequestHandler.end_headers(self)
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
         self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.end_headers()
+
+    def do_OPTIONS(self):           
+        self.send_response(200, "ok")       
+        self.send_header('Access-Control-Allow-Origin', '*')                
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")        
+
+    def do_GET(self):           
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-type',    'text/html')                                    
+        self.end_headers()              
+        self.wfile.write("<html><body>Hello world!</body></html>")
+        self.connection.shutdown(1) 
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+
+        qry_seq_raw, ref_seq_raw = post_data.split("+")
+
+        dnds_data = dnds.dnds_pipeline(qry_seq_raw, ref_seq_raw)
+
+        #pdb.set_trace()
+
+        self._set_headers()
+        self.wfile.write("<html><body><h1>"+str(dnds_data)+"</h1></body></html>")
 
 if __name__ == '__main__':
     BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
-
-
