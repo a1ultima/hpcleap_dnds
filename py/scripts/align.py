2ecd2513675e4995d10f22b10383831a7541aea6
Wade2016 <Wade2016@users.noreply.github.com>
2017-01-12 23:01:20 +0100

    Removed the while loop
    It is not necessary actually.

--- py/scripts/align.py
+++ py/scripts/align.py
@@ -181,15 +181,12 @@ def trim_gaps_from_aligned_seqs( qry_seq_aln, ref_seq_aln ):
 
    # Triming gaps and adjusting the qry indices
    qry_R=np.zeros(len(qry_seq_aln))
-   i=0
-   while qry_seq_aln[i]=='-':
-       i=i+1
-   j=qry_seq_aln[i]
+   j=0
    for i in range(len(qry_seq_aln)):
       if("-"!=qry_seq_aln[i]): 
          qry_R[i]=j
          j=j+1
-   print qry_R
+   #print qry_R
    
    qry_and_ref_arr = [(j,ref_seq_aln[i],qry_R[i]) for i,j in enumerate(qry_seq_aln) if (("-" != ref_seq_aln[i]) and ("-" != j))]
    
